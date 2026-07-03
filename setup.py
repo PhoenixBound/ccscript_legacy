1975e66bc85629d9407a439497caf7f2f42d318e
PhoenixBound <phoenixbound.github@gmail.com>
2026-07-02 20:41:08 -0500

    Use std::filesystem instead of pre-C++17 alternatives
    It's been 9 years. All the compilers support C++17 well now, 100%.
    Visual Studio 2022 still supports Windows 7, so that part's fine.
    gcc 8 and the associated libstdc++ version were released in May 2018;
    Debian oldoldstable (bullseye as of writing) has gcc 10, and my
    understanding is that Debian is normally considered to be pretty late to
    get new versions of things.
    I'm not expecting this to get merged right away, but it'd nice to
    delete code, once that does become realistic.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ extra_compile_args = []
 extra_link_args = []
 
 if platform.system() == "Linux" or platform.system() == "Darwin":
-    extra_compile_args = ["-std=c++11"]
+    extra_compile_args = ["-std=c++17"]
 
 setup(name="ccscript",
     version="1.500",
