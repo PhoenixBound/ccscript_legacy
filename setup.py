e443066ab4e367e2f2c894dd4243032b0f55e2ce
PhoenixBound <phoenixbound.github@gmail.com>
2026-07-05 18:20:44 -0500

    Directly check the compiler that setuptools uses instead of guessing
    As far as I can tell, all of these interfaces are documented parts of
    either setuptools or distutils. The linked GitHub issues currently offer
    no advice for avoiding "ccompiler" APIs, and the linked documentation
    recommends inheritance from the build_ext command class to extend
    things.
    
    The setuptools pip package is supposed to provide the 'distutils' import
    package, so this should work even on Python versions where distutils has
    been removed, in theory.

--- setup.py
+++ setup.py
@@ -1,20 +1,45 @@
 #!/usr/bin/python3
 
 import os
-import platform
+from distutils.ccompiler import get_default_compiler
 from setuptools import setup
+from setuptools.command.build_ext import build_ext
 from setuptools.extension import Extension
 
 source_files = [os.path.join("src", x) for x in os.listdir("src") if x.lower().endswith(".cpp")]
 
-extra_compile_args = []
-extra_link_args = []
+# As of July 2026, setuptools doesn't seem to include its own method to add compiler flags dependent
+# on the specific compiler (I legitimately don't understand how extra_compile_args is supposed to
+# work normally). But it does provide distutils' interface for checking what the compiler is, which
+# should work in a stable way until they add their own way.
+# See https://setuptools.pypa.io/en/latest/userguide/interfaces.html for info on stability tiers.
+# See https://setuptools.pypa.io/en/latest/userguide/extension.html for the recommendation to
+# inherit from existing commands to augment build functionality.
+# See https://github.com/pypa/setuptools/issues/2806 for people asking for an upgrade path for many
+# things related to creating and detecting compilers.
+# Especially this comment: https://github.com/pypa/setuptools/issues/2806#issuecomment-1367674526
+class build_ext_with_compiler_flags(build_ext):
+    def finalize_options(self):
+        super().finalize_options()
 
-if platform.python_compiler().startswith('MSC '):
-    extra_compile_args.append("/std:c++17")
-    extra_compile_args.append("/D_SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING")
-else:
-    extra_compile_args.append("-std=c++17")
+        extra_compile_args = []
+        extra_link_args = []
+        
+        compiler_type = None
+        if self.compiler is None:
+            compiler_type = get_default_compiler()
+        else:
+            compiler_type = self.compiler
+
+        if compiler_type == 'msvc':
+            extra_compile_args.append('/std:c++17')
+            extra_compile_args.append('/D_SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING')
+        else:
+            extra_compile_args.append('-std=c++17')
+        
+        for ext in self.extensions:
+            ext.extra_compile_args.extend(extra_compile_args)
+            ext.extra_link_args.extend(extra_link_args)
 
 setup(name="ccscript",
     version="1.500",
@@ -23,8 +48,8 @@ setup(name="ccscript",
     ext_modules=[
         Extension("ccscript",
                   source_files,
-                  language="c++",
-                  extra_compile_args=extra_compile_args,
-                  extra_link_args=extra_link_args
+                  language="c++"
                   )
-    ])
+    ],
+    cmdclass={'build_ext': build_ext_with_compiler_flags}
+    )
