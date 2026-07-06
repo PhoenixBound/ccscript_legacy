c713b69bdd6befc2df608aae3e92be0944c5235b
PhoenixBound <phoenixbound.github@gmail.com>
2026-07-05 20:40:41 -0500

    Upgrade MSVC compiles to the real std::filesystem
    This leaves Unix-like operating systems alone, as a more minimal
    alternative to the other PR I made.

--- src/fs.h
+++ src/fs.h
@@ -1,10 +1,7 @@
 #pragma once
 #ifdef _WIN32
-	#ifndef _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
-	#define _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
-	#endif // !_SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
-	#include <experimental/filesystem>
-	namespace fs = std::experimental::filesystem::v1;
+	#include <filesystem>
+	namespace fs = std::filesystem;
 #else
 	#include "filesystem/filesystem_mini.h"
 	namespace fs = filesystem;
